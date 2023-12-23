116f386d4cb935d996b41539fab2d0ee3681159f
Kastet6398 <konstantin.nezhalsky@gmail.com>
2023-12-23 12:14:00 +0200

    fff

--- src/main/java/views/SecondScreen.java
+++ src/main/java/views/SecondScreen.java
@@ -15,6 +15,7 @@ public final class SecondScreen extends BaseScreen {
     protected void initComponents() {
         setLayout(null);
         JButton button = new JButton("change");
+        button.setName(Actions.CHANGE_COLOR.name());
         button.setBounds(100, 100, 200, 50);
         button.addMouseListener(controller);
         add(button);
