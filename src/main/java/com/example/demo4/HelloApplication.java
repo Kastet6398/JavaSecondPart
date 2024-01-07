2b9136baa0f014700532b04fdfc4a04e79ecbe8e
Kastet6398 <konstantin.nezhalsky@gmail.com>
2024-01-07 11:12:18 +0200

    commit

--- src/main/java/com/example/demo4/HelloApplication.java
+++ src/main/java/com/example/demo4/HelloApplication.java
@@ -11,13 +11,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 500);
         BaseController controller = fxmlLoader.getController();
-        stage.setMaximized(true);
         controller.init();
         controller.setStage(stage);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
